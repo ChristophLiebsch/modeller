4f8eaebfef79dcac193efc194bd98b986ddf70b4
ChristophLiebsch <christoph.liebsch@gmx.de>
2025-02-10 08:21:32 +0100

    added missing dependecies and moduls

---
+++ activity.py
@@ -0,0 +1,4 @@
+
+
+def get_data():
+    return 'Hallo Welt, hier ist Web-App des Modellierungs Assistenten!'
--- api-server.py
+++ api-server.py
@@ -4,7 +4,7 @@ assistent_backend = Flask(__name__)
 
 @assistent_backend.route('/hello', methods=['GET'])
 def hello_world():
-    return jsonify({"message": "Hallo Welt, hier ist der Modellierungs Assistent!"})
+    return jsonify({"message": "Hallo Welt, hier ist die API des Modellierungs Assistenten!"})
 
 if __name__ == '__main__':
     assistent_backend.run(debug=True)
