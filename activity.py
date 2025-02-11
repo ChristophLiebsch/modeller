33d8b9cbb65513c62f2d844cdedab39fb25ebd16
ChristophLiebsch <christoph.liebsch@gmx.de>
2025-02-11 07:44:07 +0100

    Added code to create dummy commandline app

--- activity.py
+++ activity.py
@@ -2,3 +2,6 @@
 
 def get_data():
     return 'Hallo Welt, hier ist die Web-App des Modellierungs Assistenten!'
+
+if __name__ == "__main__":
+    print('\n*** Hallo Welt, hier ist die Commandline-App des Modellierungs Assistenten! ***\n')
